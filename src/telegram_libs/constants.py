a4da1a1c364637430a91d94f53d24f721843812f
Andrey Gritsaenko <gricaenko.95a@gmail.com>
2025-06-03 09:52:16 +0200

    add subscription

--- src/telegram_libs/constants.py
+++ src/telegram_libs/constants.py
@@ -3,7 +3,12 @@ import os
 required_constants = []
 
 BOTS_AMOUNT = os.getenv("BOTS_AMOUNT")
+MONGO_URI = os.getenv("MONGO_URI")
+SUBSCRIPTION_DB_NAME = os.getenv("SUBSCRIPTION_DB_NAME")
+
 required_constants.append(("BOTS_AMOUNT", BOTS_AMOUNT))
+required_constants.append(("MONGO_URI", MONGO_URI))
+required_constants.append(("SUBSCRIPTION_DB_NAME", SUBSCRIPTION_DB_NAME))
 
 missing_constants = [name for name, value in required_constants if not value]
 if missing_constants:
